51bc87b3e58140049a41eb8e1ff19a4deca7f333
seby4109 <seby4109@msn.com>
2015-04-22 22:18:14 +0300

    check if widgets is array
    if the column has no widgets array you cannot add new widgets

--- src/scripts/column.js
+++ src/scripts/column.js
@@ -101,6 +101,7 @@ angular.module('adf')
         if (widget){
           // add new item and apply to scope
           $scope.$apply(function(){
+            targetColumn.widgets = targetColumn.widgets || [];
             targetColumn.widgets.splice(evt.newIndex, 0, widget);
           });
         } else {
